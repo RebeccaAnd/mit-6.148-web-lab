04b3799b48ea5bb509da85290fd257aba5c1332b
RebeccaAnd <2303782587@qq.com>
2024-01-10 12:04:44 +0800

    GET

--- client/src/components/pages/Feed.js
+++ client/src/components/pages/Feed.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { get } from "../../utilities";
 // TODO (step2): import SingleStory
+import SingleStory from "../modules/SingleStory.js";
 // TODO (step4): import NewStory
 // TODO (step6): remove SingleStory import, import Card
 
 const Feed = () => {
   // TODO (step1): define state to hold stories
+  const [stories, setStories] = useState([]);
 
   useEffect(() => {
     // TODO (step1): implement a GET call to retrieve stories,
     // and assign it to state
+    get("/api/stories").then((storyObjs) => {
+      setStories(storyObjs);
+    });
   }, []);
 
-  return <div>This is the feed!</div>;
+  return <div>
+    {JSON.stringify(stories)}
+  </div>;
   // TODO (step1): render the raw stories data from state
   // TODO (step2): render a SingleStory with hardcoded props
+
   // TODO (step3): map the state to SingleStory components
   // TODO (step4): add in the NewStory component
   // TODO (step6): use Card instead of SingleStory
